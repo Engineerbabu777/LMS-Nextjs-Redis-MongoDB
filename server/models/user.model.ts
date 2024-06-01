c0d10a2151be1ad061b83f5ef1ce4479974096cd
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-01 18:39:15 +0500

    Add: added user model

---
+++ server/models/user.model.ts
@@ -0,0 +1,85 @@
+import mongoose, { Document, Model, Schema } from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+export interface IUser extends Document {
+  email: string
+  password: string
+  name: string
+  avatar: {
+    public_id: string
+    url: string
+  }
+  role: string
+  isVerified: boolean
+  courses: Array<{ courseId: string }>
+  comparePassword: (password: string) => Promise<boolean>
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    // add all!
+    name: {
+      type: String,
+      required: [true, 'Please enter name!']
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter email!'],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value)
+        },
+        message: 'Please enter a valid email'
+      },
+      unique: true
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter your password!'],
+      minLength: [6, 'Password must contain 6 letters!'],
+      select: false
+    },
+    avatar: {
+      public_id: String,
+      url: String
+    },
+    role: {
+      type: String,
+      default: 'user'
+    },
+    isVerified: {
+      type: Boolean,
+      default: false
+    },
+    courses: [
+      {
+        courseId: String
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+)
+
+// Hash password!
+userSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+// compare password!
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password)
+}
+
+export const userModel: Model<IUser> = mongoose.model<IUser>('User', userSchema)
