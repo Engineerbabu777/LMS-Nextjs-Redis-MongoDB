a4286cdcda3596f25660068e244bdf30fe6bc208
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-08 22:29:57 +0500

    Add: edit layout

--- server/controllers/layout.controller.ts
+++ server/controllers/layout.controller.ts
@@ -67,3 +67,78 @@ export const createLayout = CatchAsyncError(
     }
   }
 )
+
+// edit layout!
+export const editLayout = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.body as any
+
+      if (type === 'Banner') {
+        const { image, title, subTitle } = req.body as any
+        const bannerData = await layoutModel.find({ type: 'Banner' })
+        if (bannerData) {
+          await cloudinary.v2.uploader.destroy(bannerData?.image?.public_id)
+        }
+
+        // upload image to cloudinary!
+        const myCloud = await cloudinary.v2.uploader.upload(image, {
+          folder: 'layout'
+        })
+
+        const banner = {
+          image: {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url
+          },
+          title,
+          subTitle
+        }
+
+        await layoutModel.findByIdAndUpdate(bannerData?._id, { banner })
+      }
+
+      if (type === 'FAQ') {
+        const { faq } = req.body as any
+        const faqItem = await layoutModel.findOne({ type: 'FAQ' })
+
+        const faqItems = await Promise.all(
+          faq.map(async (item: any) => {
+            return {
+              question: item.question,
+              answer: item.answer
+            }
+          })
+        )
+        await layoutModel.findByIdAndUpdate(faqItem?._id, {
+          type: 'FAQ',
+          faq: faqItems
+        })
+      }
+
+      if (type === 'Categories') {
+        const { categories } = req.body as any
+        const cItem = await layoutModel.findOne({ type: 'Categories' })
+
+        const categoriesItems = await Promise.all(
+          categories.map(async (item: any) => {
+            return {
+              title: item.title
+            }
+          })
+        )
+        await layoutModel.findByIdAndUpdate(cItem?._id, {
+          type: 'Categories',
+          faq: categoriesItems
+        })
+      }
+
+      res.status(200).json({
+        success: true,
+        message: 'Layout updated successfully!'
+      })
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400))
+    }
+  }
+)
--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -3,8 +3,7 @@
 import express from 'express'
 import { authorizeRoles, isAuthenticated } from '../middleware/auth'
 
-import { createLayout } from '../controllers/layout.controller'
-
+import { createLayout, editLayout } from '../controllers/layout.controller'
 
 export const layoutRouter = express.Router()
 
@@ -13,4 +12,11 @@ layoutRouter.get(
   isAuthenticated,
   authorizeRoles('admin') as any,
   createLayout
+)
+
+layoutRouter.get(
+  '/get-all-notifications',
+  isAuthenticated,
+  authorizeRoles('admin') as any,
+  editLayout
 )
