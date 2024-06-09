80d6407ecaad089d87f1990866ff04c2ce72a019
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-09 06:38:17 +0500

    Add: theme provider added, layout fonts, meta data useful compoent created

---
+++ client/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+	open: boolean;
+	setOpen: (open: boolean) => void;
+	activeItem: number;
+};
+
+export default function Header({}: Props) {
+	return <></>;
+}
