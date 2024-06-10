821a49355cc1f0d498836a077fbf825b852a164b
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-10 23:57:37 +0500

    add: created custom modal component only

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,25 +1,34 @@
-"use client";
+'use client'
 
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import { Heading } from "@/utils/Heading";
-import { useState } from "react";
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import { Heading } from '@/utils/Heading'
+import { useState } from 'react'
 
 interface Props {}
-export default function Home({}: Props) {
-	const [open, setOpen] = useState(false);
-	const [activeItem, setActiveItem] = useState(0);
-	return (
-		<>
-			<div className="">
-				<Heading
-					title="Babu Learning"
-					description="Platform to become a Software Engineer"
-					keywords="Programming,MERN,Redux"
-				/>
-				<Header open={open} activeItem={activeItem} setOpen={setOpen} />
-			<Hero />
-			</div>
-		</>
-	);
+export default function Home ({}: Props) {
+  const [open, setOpen] = useState(false)
+  const [activeItem, setActiveItem] = useState(0)
+
+  const [route, setRoute] = useState('Login')
+
+  return (
+    <>
+      <div className=''>
+        <Heading
+          title='Babu Learning'
+          description='Platform to become a Software Engineer'
+          keywords='Programming,MERN,Redux'
+        />
+        <Header
+          setRoute={setRoute}
+          route={route}
+          open={open}
+          activeItem={activeItem}
+          setOpen={setOpen}
+        />
+        <Hero />
+      </div>
+    </>
+  )
 }
